a079558408f21407f5c83e0db996ef4cd111e1f9
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2014-01-23 09:58:12 +0100

    Fixes #5. Plugin setting for toggling public registration is not being saved

--- actions/settings/save.php
+++ actions/settings/save.php
@@ -7,6 +7,7 @@ $plugin->setSetting('debug_mode', get_input('debug_mode', false));
 $plugin->setSetting('providers', serialize(get_input('providers', array())));
 $plugin->setSetting('email_credentials', get_input('email_credentials', 'yes'));
 $plugin->setSetting('registration_instructions', get_input('registration_instructions'));
+$plugin->setSetting('public_auth', get_input('public_auth', true));
 
 system_message(elgg_echo('plugins:settings:save:ok', array($plugin_name)));
 forward(REFERER);
